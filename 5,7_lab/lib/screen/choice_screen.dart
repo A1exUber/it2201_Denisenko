81845942f927cbb85072d62570c94c6525540b79
A1exUber <163454172+A1exUber@users.noreply.github.com>
2025-03-25 14:18:59 +0300

    Закоментировал код в choice_screen.dart

--- 5,7_lab/lib/screen/choice_screen.dart
+++ 5,7_lab/lib/screen/choice_screen.dart
@@ -1,29 +1,36 @@
 import 'package:flutter/material.dart';
 
 class ChoiceScreen extends StatelessWidget {
-  const ChoiceScreen({super.key});
+  const ChoiceScreen({super.key}); // Конструктор с необязательным ключом
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Выбор'), backgroundColor: Color.fromARGB(255, 157, 28, 221),),
+      // Верхняя панель приложения (AppBar)
+      appBar: AppBar(
+        title: const Text('Выбор'), // Заголовок экрана
+        backgroundColor: Color.fromARGB(255, 157, 28, 221), // Фиолетовый цвет фона
+      ),
+      // Основное содержимое экрана
       body: Center(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center, // Центрирование по вертикали
           children: [
+            // Кнопка "Да"
             ElevatedButton(
-              onPressed: () => Navigator.pop(context, 'Да'),
-              child: const Text('Да'),
+              onPressed: () => Navigator.pop(context, 'Да'), // При нажатии возвращаем 'Да'
+              child: const Text('Да'), // Текст кнопки
               style: ElevatedButton.styleFrom(
-              backgroundColor: Color.fromARGB(255, 157, 28, 221),
-     ),
+                backgroundColor: Color.fromARGB(255, 157, 28, 221), // Фиолетовый цвет кнопки
+              ),
             ),
+            // Кнопка "Нет"
             ElevatedButton(
-              onPressed: () => Navigator.pop(context, 'Нет'),
-              child: const Text('Нет'),
+              onPressed: () => Navigator.pop(context, 'Нет'), // При нажатии возвращаем 'Нет'
+              child: const Text('Нет'), // Текст кнопки
               style: ElevatedButton.styleFrom(
-              backgroundColor: Color.fromARGB(255, 157, 28, 221),
-     ),
+                backgroundColor: Color.fromARGB(255, 157, 28, 221), // Фиолетовый цвет кнопки
+              ),
             ),
           ],
         ),
